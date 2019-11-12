dff61e8c938401d1a31ab70b55972d89f6351500
Sergi Granell <xerpi.g.12@gmail.com>
2019-11-12 22:58:32 +0100

    LCD: Read DDB

--- libbaremetal/src/lcd.c
+++ libbaremetal/src/lcd.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "lcd.h"
 #include "pervasive.h"
 #include "spi.h"
@@ -15,13 +16,13 @@
 #define DELAY 0x0D
 #define END 0xFF
 
-struct lcd_cmd {
+struct lcd_spi_cmd {
 	unsigned char cmd;
 	unsigned char size;
 	unsigned char data[];
 };
 
-struct lcd_bl_cmd {
+struct lcd_i2c_bl_cmd {
 	unsigned char reg;
 	unsigned char data;
 };
@@ -60,7 +61,36 @@ static const unsigned char lcd_bl_reset_cmd_4[] = {
 	END, END
 };
 
-static void lcd_write_cmd(const struct lcd_cmd *cmd)
+static const unsigned char lcd_cmd_disp_on_1[] = {
+	0x0D, 0x04, 0x11, 0x00, 0x0D, 0x0D,
+	0xB0, 0x01, 0x00,
+	0xC0, 0x07, 0x03, 0x21, 0x02, 0x23, 0x02, 0x07, 0x07,
+	0xB0, 0x01, 0x03,
+	0x29, 0x00,
+	0x0D, 0x01, 0xFF,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+static const unsigned char lcd_cmd_disp_on_2[] = {
+	0x0D, 0x04, 0x11, 0x00, 0x0D, 0x0D,
+	0xB0, 0x01, 0x00,
+	0xC0, 0x07, 0x03, 0x1F, 0x02, 0x23, 0x02, 0x07, 0x07,
+	0xB0, 0x01, 0x03,
+	0x29, 0x00,
+	0x0D, 0x01, 0xFF,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+static const unsigned char lcd_cmd_disp_on_3[] = {
+	0x0D, 0x04, 0x11, 0x00, 0x0D, 0x0D,
+	0xB0, 0x01, 0x00,
+	0xBB, 0x03, 0x08, 0xFF, 0x01,
+	0xB0, 0x01, 0x03,
+	0x0D, 0x04, 0x29, 0x00, 0x0D, 0x01,
+	0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+static void lcd_spi_write_cmd(const struct lcd_spi_cmd *cmd)
 {
 	unsigned int i;
 	volatile unsigned int *spi_regs = SPI_REGS(2);
@@ -120,30 +150,28 @@ static void lcd_write_cmd(const struct lcd_cmd *cmd)
 		};
 	}
 
-	while (spi_regs[0xB] && spi_regs[0xB])
-		;
+	spi_regs[4] = 1;
 
-	while (spi_regs[0xA])
-		spi_regs[0];
+	while (spi_regs[0xB])
+		;
 
 	spi_regs[4] = 0;
 
 	while (spi_regs[4] & 1)
 		;
-
-	spi_regs[2] = 0x30001;
-	spi_regs[2];
 	dsb();
 
 	pervasive_clock_disable_spi(2);
 }
 
-static void lcd_read(unsigned char *buffer, unsigned int size, int header_bits)
+static void lcd_spi_read(unsigned char *buffer, unsigned int size, int header_bits)
 {
 	volatile unsigned int *spi_regs = SPI_REGS(2);
 	unsigned int readcnt = 0;
 	unsigned int tmp_bits = 0;
 	unsigned int tmp = 0;
+
+	pervasive_clock_enable_spi(2);
 
 	while (readcnt < size) {
 		while (spi_regs[0xA] == 0)
@@ -166,11 +194,13 @@ static void lcd_read(unsigned char *buffer, unsigned int size, int header_bits)
 			tmp_bits -= 8;
 		}
 	}
+
+	pervasive_clock_disable_spi(2);
 }
 
-static void lcd_write_cmdlist(const struct lcd_cmd *cmdlist)
+static void lcd_spi_write_cmdlist(const struct lcd_spi_cmd *cmdlist)
 {
-	const struct lcd_cmd *cmd = cmdlist;
+	const struct lcd_spi_cmd *cmd = cmdlist;
 	int delay_count = 0;
 
 	while (cmd) {
@@ -181,8 +211,8 @@ static void lcd_write_cmdlist(const struct lcd_cmd *cmdlist)
 				delay_count = cmd->size - 1;
 				cmd++;
 			} else {
-				lcd_write_cmd(cmd);
-				cmd = (struct lcd_cmd *)&cmd->data[cmd->size];
+				lcd_spi_write_cmd(cmd);
+				cmd = (struct lcd_spi_cmd *)&cmd->data[cmd->size];
 			}
 		} else {
 			delay_count--;
@@ -191,7 +221,7 @@ static void lcd_write_cmdlist(const struct lcd_cmd *cmdlist)
 	}
 }
 
-static void lcd_bl_write_cmdlist(const struct lcd_bl_cmd *cmdlist)
+static void lcd_i2c_bl_write_cmdlist(const struct lcd_i2c_bl_cmd *cmdlist)
 {
 	while (cmdlist->reg != END) {
 		i2c_transfer_write(1, LCD_BACKLIGHT_I2C_ADDR, (void *)cmdlist, 2);
@@ -201,12 +231,12 @@ static void lcd_bl_write_cmdlist(const struct lcd_bl_cmd *cmdlist)
 
 static void lcd_bl_reset(void)
 {
-	struct lcd_bl_cmd *cmdlist;
+	struct lcd_i2c_bl_cmd *cmdlist;
 	unsigned int hw_info = sysroot_get_hw_info() & 0xffff00;
 	unsigned int hw_rev = hw_info - 0x804000;
 
 	if (sysroot_is_au_codec_ic_conexant()) {
-		cmdlist = (struct lcd_bl_cmd *)lcd_bl_reset_cmd_3;
+		cmdlist = (struct lcd_i2c_bl_cmd *)lcd_bl_reset_cmd_3;
 	} else {
 		if (hw_rev != 0)
 			hw_rev = 1;
@@ -216,63 +246,57 @@ static void lcd_bl_reset(void)
 
 		if (hw_rev == 0) {
 			if (hw_info < 0x901000)
-				cmdlist = (struct lcd_bl_cmd *)lcd_bl_reset_cmd_2;
+				cmdlist = (struct lcd_i2c_bl_cmd *)lcd_bl_reset_cmd_2;
 			else
-				cmdlist = (struct lcd_bl_cmd *)lcd_bl_reset_cmd_1;
+				cmdlist = (struct lcd_i2c_bl_cmd *)lcd_bl_reset_cmd_1;
 		} else {
-			cmdlist = (struct lcd_bl_cmd *)lcd_bl_reset_cmd_4;
+			cmdlist = (struct lcd_i2c_bl_cmd *)lcd_bl_reset_cmd_4;
 		}
 	}
 
-	lcd_bl_write_cmdlist(cmdlist);
+	lcd_i2c_bl_write_cmdlist(cmdlist);
 }
 
-static const unsigned char lcd_cmd_disp_on_1[] = {
-	0x0D, 0x04, 0x11, 0x00, 0x0D, 0x0D,
-	0xB0, 0x01, 0x00,
-	0xC0, 0x07, 0x03, 0x21, 0x02, 0x23, 0x02, 0x07, 0x07,
-	0xB0, 0x01, 0x03,
-	0x29, 0x00,
-	0x0D, 0x01, 0xFF,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-};
-
-static const unsigned char lcd_cmd_disp_on_2[] = {
-	0x0D, 0x04, 0x11, 0x00, 0x0D, 0x0D,
-	0xB0, 0x01, 0x00,
-	0xC0, 0x07, 0x03, 0x1F, 0x02, 0x23, 0x02, 0x07, 0x07,
-	0xB0, 0x01, 0x03,
-	0x29, 0x00,
-	0x0D, 0x01, 0xFF,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-};
-
-static const unsigned char lcd_cmd_disp_on_3[] = {
-	0x0D, 0x04, 0x11, 0x00, 0x0D, 0x0D,
-	0xB0, 0x01, 0x00,
-	0xBB, 0x03, 0x08, 0xFF, 0x01,
-	0xB0, 0x01, 0x03,
-	0x0D, 0x04, 0x29, 0x00, 0x0D, 0x01,
-	0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-};
-
-static void lcd_display_on(void)
+static void lcd_display_on(unsigned short supplier_elective_data)
 {
-	static const unsigned short supplier_elective_data = 0;
-	struct lcd_cmd *cmdlist;
+	struct lcd_spi_cmd *cmdlist;
 
 	if (supplier_elective_data < 0x25) {
-		cmdlist = (struct lcd_cmd *)lcd_cmd_disp_on_2;
+		cmdlist = (struct lcd_spi_cmd *)lcd_cmd_disp_on_2;
 	} else {
 		if (supplier_elective_data < 0x32)
-			cmdlist = (struct lcd_cmd *)lcd_cmd_disp_on_1;
+			cmdlist = (struct lcd_spi_cmd *)lcd_cmd_disp_on_1;
 		else
-			cmdlist = (struct lcd_cmd *)lcd_cmd_disp_on_3;
+			cmdlist = (struct lcd_spi_cmd *)lcd_cmd_disp_on_3;
 	}
+
+	lcd_spi_write_cmdlist(cmdlist);
+}
+
+static void lcd_read_ddb(unsigned short *supplier_id,
+			 unsigned short *supplier_elective_data)
+{
+	static const struct lcd_spi_cmd read_ddb_cmd = {
+		.cmd = 0xA1,
+		.size = 6,
+		.data = {0, 0, 0, 0, 0, 0}
+	};
+
+	unsigned char ddb[5];
+
+	lcd_spi_write_cmd(&read_ddb_cmd);
+	lcd_spi_read(ddb, sizeof(ddb), 9);
+
+	if (supplier_id)
+		*supplier_id = ddb[0] | (ddb[1] << 8);
+	if (supplier_elective_data)
+		*supplier_elective_data = ddb[2] | (ddb[3] << 8);
 }
 
 int lcd_init(void)
 {
+	unsigned short supplier_elective_data;
+
 	spi_init(2);
 	pervasive_clock_disable_spi(2);
 
@@ -281,15 +305,11 @@ int lcd_init(void)
 	gpio_port_set(0, GPIO_PORT_OLED_LCD);
 	gpio_port_set(1, 7);
 
+	lcd_read_ddb(NULL, &supplier_elective_data);
+
 	lcd_bl_reset();
 
-	lcd_display_on();
-
-	/*lcd_write_cmdlist((struct lcd_cmd *)lcd_init_cmdlist1);
-	lcd_write_cmdlist((struct lcd_cmd *)lcd_init_cmdlist3);
-	lcd_write_cmdlist((struct lcd_cmd *)lcd_disp_on_cmdlist);
-	lcd_write_cmdlist((struct lcd_cmd *)lcd_color_space_mode_0_cmdlist);
-	lcd_write_cmdlist((struct lcd_cmd *)lcd_brightness_cmdlist);*/
+	lcd_display_on(supplier_elective_data);
 
 	return 0;
 }
